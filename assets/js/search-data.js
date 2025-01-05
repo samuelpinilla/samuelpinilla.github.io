ad06a90f0607d36871b38f885e561a8be19fefd1
samuelpinilla <47015971+samuelpinilla@users.noreply.github.com>
2025-01-05 01:56:09 +0000

    Deploying to gh-pages from @ samuelpinilla/samuelpinilla.github.io@2a05fa49c2eb9cdf4005478f2d26e858061782c4 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -492,7 +492,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%73%61%6D%75%65%6C.%70%69%6E%69%6C%6C%61@%73%74%66%63.%61%63.%75%6B", "_blank");
         },
       },{
         id: 'social-inspire',
@@ -513,7 +513,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=yGayy7sAAAAJ", "_blank");
         },
       },{
         id: 'social-custom_social',
